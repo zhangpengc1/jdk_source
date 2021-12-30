0ddaaa95bc5e2beb3aca388c46f587f3c0876cdc
zhangpengc1 <31123218+zhangpengc1@users.noreply.github.com>
2021-12-30 19:35:38 +0800

    初始化提交

---
+++ src/main/java/com/marton/base/num/MaByte.java
@@ -0,0 +1,49 @@
+package com.marton.base.num;
+
+/**
+ * @author ：zhangp
+ * @Date ：2021-12-30 15:17
+ */
+public class MaByte extends MaNumber {
+
+    public static void main(String[] args) {
+            Byte bye = new Byte("100");
+
+
+
+
+    }
+
+    /************** 源码分析 1.缓存*****************/
+    /**
+     * 缓存了 -127 - 128的值在一个Byte数组里
+     * cache[0] -127
+     */
+    private static class ByteCache {
+        private ByteCache() {
+        }
+
+        static final Byte cache[] = new Byte[-(-128) + 127 + 1];
+
+        static {
+            for (int i = 0; i < cache.length; i++)
+                cache[i] = new Byte((byte) (i - 128));
+        }
+    }
+//    取的时候，直接用 b + 128的偏移量
+//    public static Byte valueOf(byte b) {
+//        final int offset = 128;
+//        return Byte.ByteCache.cache[(int)b + offset];
+//    }
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/marton/base/num/MaNumber.java
+++ src/main/java/com/marton/base/num/MaNumber.java
@@ -5,4 +5,28 @@ package com.marton.base.num;
  * @Date ：2021-12-30 14:26
  */
 public class MaNumber {
+
+    /**
+     * Number 说明
+     * <p>
+     * 抽象类 {@code Number} 是平台的超类，表示可转换为数值的类
+     * 原始类型 {@code byte}、{@code double}、{@code float}、
+     * {@code int}、{@code long} 和 {@code short}。
+     * <p>
+     * 从数值转换的具体语义
+     * 给定原语的特定 {@code Number} 实现
+     * 类型由相关的 {@code Number} 实现定义。
+     * <p>
+     * 对于平台类，转换通常类似于缩小原语转换或扩大原语转换
+     * 如 <cite>The Java&trade; 中定义的那样语言规范</cite>
+     * 用于原始类型之间的转换。因此，转换可能
+     * 丢失有关数值整体大小的信息，可能失去精度，甚至可能返回不同符号的结果。
+     */
+    public static void main(String[] args) {
+
+
+    }
+
+
+
 }
