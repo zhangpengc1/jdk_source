0c0dc21e0658bbd0da1c73550ee9bb8ce0819982
zhangpengc1 <31123218+zhangpengc1@users.noreply.github.com>
2022-02-19 16:20:20 +0800

    初始化提交

--- src/main/java/com/marton/base/num/MyInteger.java
+++ src/main/java/com/marton/base/num/MyInteger.java
@@ -1,5 +1,7 @@
 package com.marton.base.num;
 
+import java.util.HashMap;
+
 /**
  * @author ：zhangp
  * @Date ：2022-01-04 15:58
@@ -8,7 +10,12 @@ public class MyInteger {
 
     public static void main(String[] args) {
 
-        String string = Integer.toString(1000);
+        //String string = Integer.toString(1000);
+
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("1","11");
+        hashMap.put("2","22");
+        hashMap.put("3","33");
     }
 
     // 缓存
