896fa36667530685ffacdd83ece5d26e7ca7a50d
zhangpengc1 <31123218+zhangpengc1@users.noreply.github.com>
2022-02-22 16:51:28 +0800

    初始化提交

---
+++ src/main/java/com/marton/base/juc/AQSDemo.java
@@ -0,0 +1,4 @@
+package com.marton.base.juc;
+
+public class AQSDemo {
+}
---
+++ src/main/java/com/marton/redis/struct/sds/Sds.java
@@ -0,0 +1,16 @@
+package com.marton.redis.struct.sds;
+
+/**
+ * @author ：zhangp
+ * @Date ：2022/2/19 下午10:42
+ */
+public class Sds {
+
+    private int len;
+
+    private int free;
+
+    private char[] buf;
+
+
+}
